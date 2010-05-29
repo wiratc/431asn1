4bc5dc4dc1d5fbaeb4fba767f3b33fe018f8f0d6
= <=>
2010-05-29 01:23:34 -0700

    all files

---
+++ input.h
@@ -0,0 +1,11 @@
+#define AUROR 1
+#define DEATH 2
+
+#define MAX_SHOP 1000
+#define MAX_NAME 50
+#define MAX_LINE 1000
+#define MAX_WIZARD 200
+
+void input_init();
+void input_add_shop();
+int input_shop_index();
---
+++ wizard.h
@@ -0,0 +1,1 @@
+void *wizard_init(void *);
